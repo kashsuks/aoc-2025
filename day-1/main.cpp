312794e497f308ce091e553f45f75e8e3762636c
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2025-12-01 00:15:42 -0500

    added day 1 solution

--- day-1/main.cpp
+++ day-1/main.cpp
@@ -3,7 +3,6 @@ Author: kashsuks
 */
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 #define ar array
@@ -18,17 +17,38 @@ const ll INF = 1e9;
 const ld EPS = 1e-9;
 
 void solve() {
-    
+    char dir;
+    int amt;
+    int pos = 50;
+    ll p1 = 0, p2 = 0;
+
+    while (cin >> dir >> amt) {
+        for (int i = 0; i < amt; i++) {
+            if (dir == 'L') {
+                pos--;
+                if (pos < 0) pos = 99;
+            } else if (dir == 'R') {
+                pos++;
+                if (pos > 99) pos = 0;
+            }
+
+            if (pos == 0) p2++;
+        }
+
+        if (pos == 0) p1++;
+    }
+
+    cout << p1 << "\n" << p2 << "\n";
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
-    cin.tie(0); 
+    cin.tie(0);
     cout.tie(0);
+
     int tc = 1;
     // cin >> tc;
     for (int t = 1; t <= tc; t++) {
-        // cout << "Case #" << t << ": ";
         solve();
     }
 }
