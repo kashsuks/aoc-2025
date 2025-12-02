76b0c9d59a4384615e015942ab2bcbdb3b73a665
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2025-12-02 09:00:12 -0500

    added day 2 solution

--- day-2/main.cpp
+++ day-2/main.cpp
@@ -3,7 +3,6 @@ Author: kashsuks
 */
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 #define ar array
@@ -17,8 +16,36 @@ const ll MOD = 1e9 + 7;
 const ll INF = 1e9;
 const ld EPS = 1e-9;
 
+bool isDouble(ll x) {
+    string s = to_string(x);
+    int m = s.size();
+    if (m % 2) return false;
+    string a = s.substr(0, m / 2);
+    string b = s.substr(m / 2);
+    return a == b;
+}
+
 void solve() {
-    
+    string s;
+    cin >> s;
+    vector<pair<ll,ll>> v;
+    string cur = "";
+    for (char c : s) {
+        if (c == ',') {
+            int p = cur.find('-');
+            ll l = stoll(cur.substr(0, p));
+            ll r = stoll(cur.substr(p + 1));
+            v.push_back({l, r});
+            cur = "";
+        } else cur += c;
+    }
+    ll ans = 0;
+    for (auto &p : v) {
+        for (ll x = p.first; x <= p.second; x++) {
+            if (isDouble(x)) ans += x;
+        }
+    }
+    cout << ans;
 }
 
 int main() {
