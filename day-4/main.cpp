a075158fa0a3e5984f322461499dc3d7f84ee65a
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2025-12-05 23:52:07 -0500

    added day 5 solution

--- day-4/main.cpp
+++ day-4/main.cpp
@@ -18,7 +18,52 @@ const ll INF = 1e9;
 const ld EPS = 1e-9;
 
 void solve() {
-    
+    vector<string> grid;
+    string line;
+    while (cin >> line) {
+        grid.push_back(line);
+    }
+
+    int rows = sza(grid);
+    int cols = sza(grid[0]);
+    ll ans = 0;
+    bool running = true;
+
+    int dr[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+    int dc[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+    while (running) {
+        running = false;
+        vector<pair<int, int>> toRemove;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == '@') {
+                    int neighbors = 0;
+                    for (int i = 0; i < 8; i++) {
+                        int nr = r + dr[i];
+                        int nc = c + dc[i];
+                        if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == '@') {
+                            neighbors++;
+                        }
+                    }
+                    if (neighbors < 4) {
+                        toRemove.push_back({r, c});
+                    }
+                }
+            }
+        }
+
+        if (!toRemove.empty()) {
+            running = true;
+            ans += sza(toRemove);
+            for (auto p : toRemove) {
+                grid[p.first][p.second] = '.';
+            }
+        }
+    }
+
+    cout << ans << endl;
 }
 
 int main() {
--- day-5/main.cpp
+++ day-5/main.cpp
@@ -18,7 +18,44 @@ const ll INF = 1e9;
 const ld EPS = 1e-9;
 
 void solve() {
-    
+    vector<pair<ll, ll>> ranges;
+    string line;
+
+    while (getline(cin, line)) {
+        if (line.empty()) break;
+
+        size_t dashPos = line.find('-');
+        if (dashPos == string::npos) continue;
+
+        ll start = stoll(line.substr(0, dashPos));
+        ll end = stoll(line.substr(dashPos + 1));
+
+        ranges.push_back({start, end});
+    }
+
+    if (ranges.empty()) {
+        cout << 0 << endl;
+        return;
+    }
+
+    sort(all(ranges));
+
+    ll total = 0;
+    ll curStart = ranges[0].first;
+    ll curEnd = ranges[0].second;
+
+    for (int i = 1; i < sza(ranges); i++) {
+        if (ranges[i].first <= curEnd) {
+            curEnd = max(curEnd, ranges[i].second);
+        } else {
+            total += (curEnd - curStart + 1);
+            curStart = ranges[i].first;
+            curEnd = ranges[i].second;
+        }
+    }
+
+    total += (curEnd - curStart + 1);
+    cout << total << endl;
 }
 
 int main() {
